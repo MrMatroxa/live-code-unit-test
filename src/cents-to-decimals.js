001b33653aafdf2b945ddd7d101b5bf4f6287781
omardieh <omardieh@gmail.com>
2024-01-12 12:30:19 +0100

    unit test

---
+++ src/cents-to-decimals.js
@@ -0,0 +1,17 @@
+function centsToDecimals(cents) {
+  return cents / 100;
+}
+
+//
+// function centsToDecimals(cents) {
+//   return cents / 100;
+// }
+// console.log(
+//   "\nShould convert value from cents to a floating point number with 2 decimals"
+// );
+// console.log(centsToDecimals(105) === 1.05);
+// console.log(centsToDecimals(23) === 0.23);
+
+// make the test fail
+// make the test pass
+// refactor
